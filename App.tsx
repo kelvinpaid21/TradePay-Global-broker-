089b62aafa365425f8608b6a1501d5ca8cb1b6d6
kelvinpaid21 <kelvinpaid21@gmail.com>
2026-01-04 00:46:01 +0530

    Simplify App component to display a message
    Removed unnecessary imports and components, simplifying the App component to display a message.

--- App.tsx
+++ App.tsx
@@ -1,47 +1,5 @@
+import React from 'react';
 
-import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import MarketTicker from './components/MarketTicker';
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import HowItWorks from './pages/HowItWorks';
-import Contact from './pages/Contact';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  return null;
-};
-
-const App: React.FC = () => {
-  return (
-    <Router>
-      <ScrollToTop />
-      <div className="flex flex-col min-h-screen bg-slate-950 text-slate-100">
-        <MarketTicker />
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/how-it-works" element={<HowItWorks />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/signin" element={<SignIn />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
-};
+const App = () => <h1 style={{ textAlign: 'center', marginTop: '50px' }}>Hello, Vercel is working!</h1>;
 
 export default App;
